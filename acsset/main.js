9620750b754a8d892c43610e1b19fba83848cb0f
anhthai2113 <anhthaiblockchain21@gmail.com>
2025-06-27 15:39:53 +0700

    javascript

---
+++ acsset/main.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 3;
+
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+console.log("Chia lấy dư:", a % b);
+
+
+console.log("a == b:", a == b);       
+console.log("a === b:", a === b);     
+console.log("a != b:", a != b);      
+console.log("a !== b:", a !== b);     
+console.log("a < b:", a < b);         
+console.log("a > b:", a > b);         
+console.log("a <= b:", a <= b);       
+console.log("a >= b:", a >= b);   
