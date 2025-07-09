bf5c6eeaac29f65942ce4b0fe76be858c6a2f210
anhthai2113 <anhthaiblockchain21@gmail.com>
2025-07-09 14:25:36 +0700

    DOM

--- acsset/main.js
+++ acsset/main.js
@@ -1,21 +1,9 @@
-function checkNumber(num) {
-    return new Promise((resolve, reject) => {
-        if (num % 2 === 0) {
-            resolve(`Số ${num} là số chẵn!`);
-        } else {
-            reject(`Lỗi: Số ${num} là số lẻ!`);
-        }
-    });
-}
-let inputNumber = 5; 
-
-checkNumber(inputNumber)
-    .then((message) => {
-        console.log("✅ Thành công:", message);
-    })
-    .catch((error) => {
-        console.error("❌ Lỗi:", error);
-    })
-    .finally(() => {
-        console.log("👉 Đã kiểm tra xong.");
-    });
+const title = document.getElementById("main__title");
+    title.innerText = "Chào Mừng Bạn Đến Với Khóa Học JavaScript!";
+    const description = document.querySelector(".description");
+    description.innerHTML = "Bạn sẽ học về <strong>DOM, Events</strong>";
+    function showGreeting() {
+      const name = document.getElementById("student__name").value;
+      const greeting = document.getElementById("greeting");
+      greeting.innerText = `Xin chào, ${name}!`;
+    }
